893c319709f2ea0270231ed26a618189804195eb
Mohin Pathan <mohinpathan2004@gmail.com>
2026-05-05 13:10:24 +0530

    Added

--- 05_MiddelWares/app.js
+++ 05_MiddelWares/app.js
@@ -36,7 +36,6 @@ app.use((req,res)=>{
 app.use((err,req,res,next)=>{
     console.log(err.message)
 
-
     res.status(error.statusCode || 500)
     .json({message :error.message || "Internal server error"})
 })
