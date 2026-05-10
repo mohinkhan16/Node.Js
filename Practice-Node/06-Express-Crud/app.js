72e5822e03c22bdab34dd3e07e452bb1a7eada23
Mohin Pathan <mohinpathan2004@gmail.com>
2026-05-10 08:37:04 +0530

    Added

--- Practice-Node/06-Express-Crud/app.js
+++ Practice-Node/06-Express-Crud/app.js
@@ -36,7 +36,56 @@ app.get("taskList",(req,res)=>{
     .json({message:"TaskList",taskList})
 })
 
+app.post("/addTask",(req,res,next)=>{
+    const { addtask }=req.body;
 
+    if( !task || !description){
+        return next(
+            new httpError("Task or Description is not requried",404)
+        );
+    }
+
+    const newTask={
+        id:new Date().getTime(),
+        task,
+        description,
+    };
+    task.push(newTask);
+
+    res.status(201).json({
+        success:true,
+        message:"New Task Added Succesfully",
+        newTask
+    });
+});
+
+app.delete("/task/:id",(req,res,next)=>{
+    const id=Number(req.params.id);
+
+    const index=task.findeIndex((t)=>t.id===id);
+
+    if(index===-1){
+        return next(
+            new httpError("Request not found",404)
+        );
+    }
+
+        task.splice(index,1);
+
+        res.status(200).json({
+            success:true,
+            message:"Task Deleted successfully"
+        });
+});
+
+app.use((req,res,next)=>{
+    next(new httpError("Request not found",404));
+
+    res.status(err.statucode || 500).json({
+        success :false,
+        message:err.message || "something went wrong"
+    });
+});
 
 const port =5000;
 
@@ -46,3 +95,4 @@ app.listen(port,(err)=>{
     }
     console.log(`surver is running on ${port}`)
 })
+
