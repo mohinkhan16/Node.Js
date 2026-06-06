85a2713de42b69889ee4815551facd4852e07b04
Mohin khan <mohinpathan2004@gmail.com>
2026-06-06 11:03:31 +0530

    some code change in model

--- 10_Distinova/model/DistinovaModel.js
+++ 10_Distinova/model/DistinovaModel.js
@@ -3,25 +3,45 @@ import mongoose from "mongoose";
 
 const DistinovaSchema=new mongoose.Schema(
     {
-        DistinovaName:{
-            type:String,
-            required:true,
-            trim:true,
-        },
-        Price :{
-            type:Number,
-            required:true,
-            min:0
-        },
-        startDate:{
-            type:Date,
-            required:true
-        },
-        endDate:{
-            type:Date,
-            required:true
-        }
-});
+           packageName: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    price: {
+      type: Number,
+      min: 0,
+      required: true,
+    },
+    startDate: {
+      type: Date,
+      required: true,
+    },
+    endDate: {
+      type: Date,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    destination: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    packageImage: {
+      type: String,
+      // required: true,
+    },
+    packageType: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  },
+);
 
 const Distinova = mongoose.model("Distinova",DistinovaSchema);
 
