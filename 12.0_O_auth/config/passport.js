a9b322920802dfc244e4f2d010141299acbd4c87
Mohin khan <mohinpathan2004@gmail.com>
2026-07-02 11:36:37 +0530

    passport js revision

---
+++ 12.0_O_auth/config/passport.js
@@ -0,0 +1,53 @@
+
+import passport from "passport";
+
+import passportGoogle from "passport-google-oauth20";
+
+import user from "../model/User.js";
+
+import dotenv from "dotenv";
+import User from "../model/User.js";
+dotenv.config({path:"./.env"});
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: process.env.CALLBACK_URL,
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+
+        if (!user) {
+          user = await User.create({
+            googleId: profile.id,
+            name: profile.displayName,
+            email: profile.emails[0].value,
+          });
+        }
+
+        return done(null, user);
+      } catch (err) {
+        return done(err, null);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
+
+export default passport;
+
